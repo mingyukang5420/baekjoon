bb15f9ecfe58b72d69ff63f9bd7594d6ec188c6e
mingyukang5420 <mingyukang@kakao.com>
2026-06-04 11:29:15 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.08 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    var answer = '';
+    
+    var length = arr.length;
+    for (i = 0; i < length; i++){
+        answer = answer + arr[i];
+    }
+    
+    
+    
+    return answer;
+}
